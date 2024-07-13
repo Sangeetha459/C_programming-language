7bafddb21db08f136be84561a069d6b5c0cf1e3f
Sangeetha459 <sangeetharamadeni@gmail.com>
2024-07-13 20:36:51 +0530

    Update cse,eceand mech programs.c

--- cse,eceand mech programs.c
+++ cse,eceand mech programs.c
@@ -11,17 +11,18 @@ void main()
     if(CSE>ECE)
     {
         printf("Highest placement CSE");
-      #include<stdio.h>
-void main()
-{
-    int CSE,ECE,MECH;
-    printf("Enter the no of students placed in CSE:");
-    scanf("%d",&CSE);
-    printf("Enter the no of students placed in ECE:");
-    scanf("%d",&ECE);
-    printf("Enter the no of students placed in MECH:");
-    scanf("%d",&MECH);
-    if(CSE>ECE)
+    }
+    else if(ECE>MECH)
     {
-        printf("Highest placement CSE");
+        printf("Highest placement ECE");
+    }
+    else
+    {
+        printf("Highest placement MECH");
+    }
+    
+}
+
+     
+       
 
