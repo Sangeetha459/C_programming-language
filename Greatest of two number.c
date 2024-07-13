2b33b131804219ed87f7a14e5d5bb6cbc1e47eb9
Sangeetha459 <sangeetharamadeni@gmail.com>
2024-07-13 16:37:21 +0530

    Create Greatest of two number.c

---
+++ Greatest of two number.c
@@ -0,0 +1,20 @@
+printf(“the even number”);
+}
+else
+{
+printf(“the odd number”);
+}
+}
+scanf(‘%d”,&a);
+printf(“b:”);
+scanf(“%d”,&b);
+If(a>b)
+{
+printf(“a value is large”);
+}
+else
+{
+printf(“b value is large”);
+}
+}
+
