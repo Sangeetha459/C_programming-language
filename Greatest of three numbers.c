ef9b8fbf547c2847d67b2562abbc182bf4e4d50b
Sangeetha459 <sangeetharamadeni@gmail.com>
2024-07-13 16:42:47 +0530

    Create Greatest of three numbers.c

---
+++ Greatest of three numbers.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+Int i ,n,sum;
+print(“n”);
+scanf(“%d”,&n);
+sum=0;
+for(i=1;i<=n;i++)
+{
+sum=sum+I;
+}
+printf(“the sum is%d”,sum);
+}
+printf(“b value is large”);
+}
+else
+{
+printf(“c value is large”);
+}
+}
+
